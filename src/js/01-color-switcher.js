b9a345868125ef313708e6dfe9876402040343bb
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-20 21:29:55 +0300

    добавляет первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
-
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let colorSwap = null;
+btnStart.addEventListener('click', event => {
+  colorSwap = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    btnStart.disabled = true;
+  }, 1000);
+});
+btnStop.addEventListener('click', event => {
+  clearInterval(colorSwap);
+  btnStart.disabled = false;
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
