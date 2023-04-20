939f4fc037a874a11a7fab7796fab0aa720232a1
PaulPonomarenko <117308982+PaulPonomarenko@users.noreply.github.com>
2023-04-20 18:30:20 +0000

    Deploying to gh-pages from @ PaulPonomarenko/goit-js-hw-09@b9a345868125ef313708e6dfe9876402040343bb 🚀

---
+++ 01-color-switcher.763be2e5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let o=null;t.addEventListener("click",(e=>{o=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(e=>{clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.763be2e5.js.map
